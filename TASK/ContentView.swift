7d609707f09833037ed94df478d3a21f1e1b38ea
FCabcasd <1608404430@qq.com>
2025-08-30 22:15:49 +0800

    Update ContentView.swift

--- TASK/ContentView.swift
+++ TASK/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Text("zjj is gay")
+            Text("fuck you")
         }
         .padding()
     }
