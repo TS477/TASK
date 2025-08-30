a7b6b932a906599d67f177473405d845d19a81ff
Andy <yyang9102@gmail.com>
2025-08-30 21:49:36 +0800

    Initial Commit

---
+++ TASK/TASKApp.swift
@@ -0,0 +1,17 @@
+//
+//  TASKApp.swift
+//  TASK
+//
+//  Created by TSOvO on 30/8/2025.
+//
+
+import SwiftUI
+
+@main
+struct TASKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
